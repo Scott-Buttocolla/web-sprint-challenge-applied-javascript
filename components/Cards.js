1336273061dbf749e07ff6d749141059a4586810
scott-buttocolla@lambdastudents.com <sbuttocolla0810@gmail.com>
2020-10-30 10:12:10 -0700

    have notes and basic structure to start building upon for cards, working on adding structure to elements and content of each card

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,36 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+
+// needing to create a function that allows me to acces the data in an array where each object is another array.
+// will need to break down the each array into a seperate array for access
+//first step is to create the parent variable that will be returned at the end
+// second step pull the api address to inspect the data
+// third step is to create a function that will add all the items to the arrays
+// calling the parameters as needed. We will be calling 
+// fourth step is to create an array for each area of the area to eventually put the data, .push is how we add items to an emtpy array.
+
+// creating the variable to later append to for the parent
+const cardContainer = document.querySelector('.card-container')
+
+function articleCreator(array, obj){
+
+// giving each needed tag an element
+    const mainDiv = document.createElement('div');
+    const titleDiv = document.createElement('div');
+    const authDiv = document.createElement('div');
+    const imgDiv = document.createElement('div');
+    const img = document.createElement('img');
+    const spanName = document.createElement('span');
+
+// giving each element class
+    mainDiv.classList.add('card');
+    titleDiv.classList.add('headline');
+    authDiv.classList.add('author');
+    imgDiv.classList.add('img-container')
+
+    // giving structure
+    
+}
+
