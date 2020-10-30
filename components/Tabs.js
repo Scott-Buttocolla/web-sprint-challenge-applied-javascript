7724d09befcb5f60d579cfe6971cb7aade871830
scott-buttocolla@lambdastudents.com <sbuttocolla0810@gmail.com>
2020-10-30 09:37:02 -0700

    working on tabs js, function is created, now I need to pull the topic titles from the node list to an array and iterate over them

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,28 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+// ^^^^^^^ made note of this in HTML file before looking through all js files
+
+// declaring topicsTabs to the parent element in HTML
+const topics = document.querySelector('.topics');
+
+// axios is pre-loaded in the HTML file. No need to npm install axios and add import axios from "axios" for access to axios
+// comment out line 11 in HTML file if axios is to be imported instead and add import line above on line 13 for readablity with proper comments
+
+function tabCreator(data){
+    // creating the element
+    const tabDiv = document.createElement('div');
+    // adding class
+    tabDiv.classList.add('tab');
+    //adding the content as the data gets passed in
+    tabDiv.textContent = data;
+    //adding the structure 
+    topics.appendChild(tabDiv);
+}
+console.log(tabCreator)
+// axios is pre-loaded in the HTML file. No need to npm install axios and add import axios from "axios" for access to axios
+// comment out line 11 in HTML file if axios is to be imported instead and add import line above on line 13 for readablity with proper comments
+
+
+
+
